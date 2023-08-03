a3d8ea1dbcf568e73c3ef505a2cfb339433a758b
sonukuldeep <sonukuldeep@outlook.com>
2023-08-03 19:36:58 +0530

    start end

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ gsap.registerPlugin(ScrollTrigger)
 gsap.to(".square", {
     x: 700,
     duration: 3,
-    scrollTrigger: ".square2"
+    scrollTrigger: {
+        trigger: ".square",
+        start: "top 80%",//400, //400(px) or top/center/bottom top/center/bottom or top/center/bottom 50% or it can be a function too
+        end: "top 50%", //() => `+=${document.querySelector(".square").offsetHeight}`
+        markers: true,
+        toggleClass: "red"
+    }
 })
