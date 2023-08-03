85b7646cda215bc05d35affd13bcebe3256a754a
sonukuldeep <sonukuldeep@outlook.com>
2023-08-03 18:06:27 +0530

    gsap highlight

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 gsap.registerPlugin(ScrollTrigger)
 
-const tl = gsap.timeline()
-
-tl.to(".box", { x: 500, duration: 5 })
-    .to(".box", { y: 500, duration: 5 })
-    .addLabel("rotate")
-    .to(".box", { rotate: 90, repeat: 2, ease: "bounce" })
-
-tl.play("rotate")
-
-ScrollTrigger.create({
-    animation: tl,
-    trigger: ".box",
-    start: "top 80%",
-    markers: true
+gsap.utils.toArray("span").forEach(span => {
+    ScrollTrigger.create({
+        trigger: span,
+        start: "top center",
+        onEnter: () => span.classList.add("active"),
+        markers: true
+    })
 })
