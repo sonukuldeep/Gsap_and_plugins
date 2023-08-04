244033a9097953edc31093e866d7aa3eedb52cf9
sonukuldeep <sonukuldeep@outlook.com>
2023-08-04 12:38:47 +0530

    standalone update

--- script.js
+++ script.js
@@ -6,4 +6,9 @@ ScrollTrigger.create({
     start: "top 80%",
     end: "top 50%",
     toggleClass: "box-red",
+    onUpdate: (self) => console.log(self),
+    // onEnter: () => console.log("enter"),
+    // onLeave: () => console.log("leave"),
+    // onEnterBack: () => console.log("enter back"),
+    // onLeaveBack: () => console.log("leave back"),
 })
