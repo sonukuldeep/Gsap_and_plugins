8d14709b98038854ffa0e84dd61a42ea853765f7
sonukuldeep <sonukuldeep@outlook.com>
2023-08-04 11:42:57 +0530

    toggle actions

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ gsap.to(".square", {
     duration: 3,
     scrollTrigger: {
         trigger: ".square",
-        start: "top 80%",//400, //400(px) or top/center/bottom top/center/bottom or top/center/bottom 50% or it can be a function too
+        start: "top 60%",//400, //400(px) or top/center/bottom top/center/bottom or top/center/bottom 50% or it can be a function too
         end: "top 50%", //() => `+=${document.querySelector(".square").offsetHeight}`
         markers: true,
-        toggleClass: "red"
+        toggleClass: "red",
+        toggleActions: "restart pause reset pause", // default is "play none none none" onEnter/ onLeave/ onEntryBack/ onLeaveBack 
+        // valid toddleActions are:- play pause restart reset reverse resume complete none  
     }
 })
