f2c7db73e64a6943d3d2e56434bf89b52938953f
sonukuldeep <sonukuldeep@outlook.com>
2023-08-04 12:09:16 +0530

    pin element

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ gsap.to(".square", {
     x: 300,
     duration: 3,
     scrollTrigger: {
-        trigger: ".square",
+        trigger: ".square2",
         start: "top 60%",//400, //400(px) or top/center/bottom top/center/bottom or top/center/bottom 50% or it can be a function too
         end: "bottom 30%", //() => `+=${document.querySelector(".square").offsetHeight}`
         markers: true,
@@ -13,5 +13,7 @@ gsap.to(".square", {
         //toggleActions: "restart pause reset pause", // default is "play none none none" onEnter/ onLeave/ onEntryBack/ onLeaveBack 
         // valid toddleActions are:- play pause restart reset reverse resume complete none  
         scrub: 1, // can be true/false or numeric value to add smoothing
+        pin: ".square", // true to pin the trigger element or pass a different class to pin that instead 
+        pinSpacing: true,
     }
 })
