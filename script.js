b9f0ec824118c867dc45ec72c66d0df763c8107b
sonukuldeep <sonukuldeep@outlook.com>
2023-08-04 12:22:35 +0530

    timeline and scroll trigger

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
 gsap.registerPlugin(ScrollTrigger)
 
-gsap.to(".square", {
-    rotation: 360,
-    x: 300,
-    duration: 3,
+
+const tl = gsap.timeline({
     scrollTrigger: {
-        trigger: ".square2",
-        start: "top 60%",//400, //400(px) or top/center/bottom top/center/bottom or top/center/bottom 50% or it can be a function too
-        end: "bottom 30%", //() => `+=${document.querySelector(".square").offsetHeight}`
+        trigger: ".box",
         markers: true,
-        toggleClass: "red",
-        //toggleActions: "restart pause reset pause", // default is "play none none none" onEnter/ onLeave/ onEntryBack/ onLeaveBack 
-        // valid toddleActions are:- play pause restart reset reverse resume complete none  
-        scrub: 1, // can be true/false or numeric value to add smoothing
-        pin: ".square", // true to pin the trigger element or pass a different class to pin that instead 
-        pinSpacing: true,
+        start: "top 80%",
+        end: "top 30%",
+        scrub: 1,
     }
 })
+tl.to(".box", { x: 500, duration: 2 })
+    .to(".box", { y: 200, duration: 3 })
+    .to(".box", { x: 0, duration: 2 })
